bc14ca8999589963b17cb8692319739449fc44b8
Jason Xie (xix277) <xiejason2@gmail.com>
2024-01-24 13:26:56 -0600

    Initial Commit

--- D1/coeff2Verilog.m
+++ D1/coeff2Verilog.m
@@ -1,4 +1,4 @@
-function coeff2Verilog(b_n, bits)
+function coeff2Verilog(b_n, bits, fileName)
 
     if rem(length(b_n),2) == 1 % check if odd
         half_length = (length(b_n)-1)/2 + 1;
@@ -9,21 +9,21 @@ function coeff2Verilog(b_n, bits)
     
     b = b_n(1 : half_length);
 
-    fileName = fopen("VC.txt", 'wt');
-    fprintf(fileName,"always @ *\n");
-    fprintf(fileName,"begin\n");
+    fid = fopen(fileName, 'wt');
+    fprintf(fid,"always @ *\n");
+    fprintf(fid,"begin\n");
     % put coefficients into a verilog file
     for i = 1:half_length
         idx = i-1;
-        if b(i) < 0
-            fprintf(fileName, "\tb[%d] = -%d'sd %d;\n", idx, bits, b(i));
+        if b(i) < 0 % check for negative
+            fprintf(fid, "\tb[%d] = -%d'sd %d;\n", idx, bits, b(i));
         else
-            fprintf(fileName, "\tb[%d] = %d'sd %d;\n", idx, bits, b(i));
+            fprintf(fid, "\tb[%d] = %d'sd %d;\n", idx, bits, b(i));
         end
 
     end
-    fprintf(fileName, "end");
+    fprintf(fid, "end");
 
-    fclose(fileName);
+    fclose(fid);
 
 end
