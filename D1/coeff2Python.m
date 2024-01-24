51b582454026ed28c23cd5163805f5d1c5034764
Jason Xie (xix277) <xiejason2@gmail.com>
2024-01-24 13:28:42 -0600

    initial commit

---
+++ D1/coeff2Python.m
@@ -0,0 +1,21 @@
+function coeff2Python(b_n, fileName, fractional_bits)
+
+    if rem(length(b_n),2) == 1 % check if odd
+        half_length = (length(b_n)-1)/2 + 1;
+    else
+        half_length = length(b_n)/2 + 1;
+    end
+    
+    
+    b = b_n(1 : half_length)*2^fractional_bits;
+    %fileName = fopen("VC.txt", 'wt');
+    fid = fopen(fileName, "wt");
+    % put coefficients into a verilog file
+    for i = 1:half_length
+        fprintf(fid, "%d\n", round(b(i)));
+
+    end
+
+    fclose(fid);
+
+end
