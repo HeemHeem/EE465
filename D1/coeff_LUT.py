2b0fbbd604de89127c2f360bf6586fbd4572a633
Jason Xie <xiejason2@gmail.com>
2024-01-28 15:51:42 -0600

    fixed LUT_outputs to account for last coefficient only needing initial inputs also modified the inputs of LUT_outputs to take in fractional bits and only input symbols in decimal instead of LUT_inputs in verilog

--- D1/coeff_LUT.py
+++ D1/coeff_LUT.py
@@ -38,7 +38,7 @@ def convert_to_verilog(in_val: list, fract_bits: int) -> list:
     return [int(round(num * 2 ** fract_bits)) for num in in_val]
 
 def get_coeff_from_txt(txt_file: str) -> list:
-    """Return a list of verilog coefficients from textfile
+    """Return a list of verilog coefficients from text file
 
     Args:
         txt_file (str): name of coefficient txt file
@@ -54,18 +54,24 @@ def get_coeff_from_txt(txt_file: str) -> list:
     return coeff_list
         
 
-def LUT_outputs(lut_inputs_verilog: list, coeff_list_verilog:list, scale_factor: int) -> dict:
+def LUT_outputs(symb_list: list, coeff_list_verilog:list, scale_factor: int, frac_bits: int) -> dict:
     """Generate a dictionary of lists for the possible verilog LUT outputs based on filter coefficients
         also scale down by a factor due to python not handling fractions well. so scale down by the input scaling
+        Reminder, the middle coefficient only needs the initial inputs, not the combo
 
     Args:
-        lut_inputs (list): list of input values in verilog format
+        symb_list (list): list of input symbols in decimal format
         coeff_list_verilog: list of coefficients in verilog format
         scale_factor: scale down by this factor since python doesn't handle fractions well
+        frac_bits: integer number of fractional bits
     
     Returns:
         dict: a dictionary of lists for each LUT
     """
+    # generate lut inputs
+    symb_list_verilog = convert_to_verilog(symb_list, frac_bits)
+    lut_in = LUT_inputs(symb_list_verilog)
+    
     # generate key names
     LUT_dict = {}
     for num in range(0,len(coeff_list_verilog)):
@@ -73,10 +79,18 @@ def LUT_outputs(lut_inputs_verilog: list, coeff_list_verilog:list, scale_factor:
         # LUT_dict[LUT_Num] = []
         # calculate lut values
         tmp = []
-        for inpt in lut_inputs_verilog:
-            mult = coeff_list_verilog[num] * inpt
-            if mult not in tmp:
+        
+        # for last coefficient -  it only requires the initial input values
+        if num == len(coeff_list_verilog)-1:
+            for inpt in symb_list_verilog:
+                mult = coeff_list_verilog[num] * inpt
                 tmp.append(mult)
+        else:
+                
+            for val in lut_in:
+                mult = coeff_list_verilog[num] * val
+                if mult not in tmp:
+                    tmp.append(mult)
         
         for idx in range(0,len(tmp)):
             tmp[idx] = round(tmp[idx]/scale_factor)
@@ -89,6 +103,14 @@ def LUT_outputs(lut_inputs_verilog: list, coeff_list_verilog:list, scale_factor:
 # TODO: write to verilog file with the list of coefficients needed. Also need to fix last LUT as it only requires the initial input values and no the combination
 # print(LUT_inputs(symbols))
 
+def generate_filter_script(LUT_Outputs:dict, file_name: str) -> None:
+    """ Write write LUT case statements to existing verilog filter script.
+
+    Args:
+        LUT_Outputs (dict): dictionary of LUT outputs
+        file_name (str): name of existing verilog filter file
+    """
+
 
 in_val_verilog = convert_to_verilog(symbols, 16)
 print(in_val_verilog)
@@ -96,10 +118,10 @@ print(in_val_verilog)
 lut_inpt_verilog = LUT_inputs(in_val_verilog)
 print(lut_inpt_verilog)
 
-coeff_list = get_coeff_from_txt("coeff_tx.txt")
+coeff_list = get_coeff_from_txt("Coeff4Python.txt")
 print(coeff_list)
 
-LUTs = LUT_outputs(lut_inpt_verilog, coeff_list, 3)
+LUTs = LUT_outputs(symbols, coeff_list, 3, 16)
 # print(LUTs["LUT_0"])
 print(LUTs)
 
