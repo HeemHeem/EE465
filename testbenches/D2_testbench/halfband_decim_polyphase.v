f45c326fdba9499a1d63071738859097ca0e7ec9
Jason Xie (xix277) <xiejason2@gmail.com>
2024-03-27 15:49:09 -0600

    Got timehsharing working with decimator

--- testbenches/D2_testbench/halfband_decim_polyphase.v
+++ testbenches/D2_testbench/halfband_decim_polyphase.v
@@ -5,7 +5,7 @@ module halfband_filter_decim(
                     output reg signed [17:0] y //1s17);
 );
 
-reg signed [17:0] y1, y1_delay, y2_acc_delay, y2_acc_delay2;
+reg signed [17:0] y1, y2_acc_delay, y2_acc_delay2;
 reg signed [35:0] y2, y2_acc;// 2s34
 reg counter, counter_lpf;
 // inputs
@@ -89,15 +89,7 @@ always @ (posedge clk or posedge reset)
 
 // filter 1
 always @ (posedge clk)
-    if( sam_clk_en)
-    y1 <= x1[2] >>> 1;
-    else
-        y1 <= y1;
-// always @ (posedge clk)
-//     if(sam_clk_en)
-//         y1_delay <= y1; 
-//     else
-//         y1_delay <= y1;
+    y1 = x1[2] >>> 1;
 
 // filter 2
 always @ *
@@ -107,41 +99,38 @@ always @ *
     h1_in = {x2[0][17], x2[0][17:1]} + {x2[3][17], x2[3][17:1]}; //2s16
 
 
+// always @ *
+//     y2 = h_mult*x_mult;
 always @ *
-    y2 = h_mult*x_mult;
-// always @ *
-//     h3_out = h3 * h3_in; // 2s34
+    h3_out = h3 * h3_in; // 2s34
 
-// always @ *
-//     h1_out = h1 * h1_in; //2s34
+always @ *
+    h1_out = h1 * h1_in; //2s34
 
-// always @ *
-//     y2 = h1_out + h3_out; // 2s34 + 2s34
+always @ *
+    y2 = h1_out + h3_out; // 2s34 + 2s34
 
 always @ (posedge clk or posedge reset)
     if (reset)
         y <= 18'sd0;
     else if (sam_clk_en)
-        y <= y2_acc_delay + y1;
+        y <= y2[34:17] + y1;
     else
         y <= y;
+// // accumulator
+// always @ (posedge clk)
+//     if(reset)
+//         y2_acc <= y2;
+//     else if (counter_lpf == 1'b0)
+//         y2_acc <= y2;
+//     else
+//         y2_acc <= y2_acc + y2;
 
+// always @ (posedge clk)
+//     y2_acc_delay <= y2_acc[34:17];
 
-// accumulator
-always @ (posedge clk)
-    if(reset)
-        y2_acc <= y2;
-    else if (counter_lpf == 1'b0) // clear
-        y2_acc <= y2;
-    else
-        y2_acc <= y2_acc + y2;
-
-
-always @ (posedge clk)
-    y2_acc_delay <= y2_acc[34:17];
-
-always @ (posedge clk)
-    y2_acc_delay2  <= y2_acc_delay;
+// always @ (posedge clk)
+//     y2_acc_delay2  <= y2_acc_delay;
 // always @ *// (posedge clk)
 //     y2 = h1_out + h3_out; // 2s34
 
@@ -156,31 +145,31 @@ always @ (posedge clk or posedge reset)
 
 always @ (posedge clk or posedge reset)
     if(reset)
-        counter_lpf <= 1'd1;
-    // else if (sam_clk_en)
-    //     counter_lpf <= 1'd0;
+        counter_lpf <= 1'd0;
+    else if (sam_clk_en)
+        counter_lpf <= 1'd0;
     else
         counter_lpf <= counter_lpf + 1'd1;
 
 
-always @ *
-    begin
-        case(counter_lpf)
-        1'b0: h_mult = h1;
-        1'b1: h_mult = h3;
-        default: h_mult = h1;
-    endcase
-    end
+// always @ *
+//     begin
+//         case(counter_lpf)
+//         1'b0: h_mult = h1;
+//         1'b1: h_mult = h3;
+//         default: h_mult = h1;
+//     endcase
+//     end
 
 
-always @ *
-    begin
-        case(counter_lpf)
-        1'b0: x_mult = h1_in;
-        1'b1: x_mult = h3_in;
-        default: x_mult = h1_in;
-    endcase
-    end
+// always @ *
+//     begin
+//         case(counter_lpf)
+//         1'b0: x_mult = h1_in;
+//         1'b1: x_mult = h3_in;
+//         default: x_mult = h1_in;
+//     endcase
+//     end
     
 // always @ *
 //     y = y1 + y2[34:17];
--- testbenches/D2_testbench/tbench.sv
+++ testbenches/D2_testbench/tbench.sv
@@ -172,23 +172,23 @@ halfband_filter_decim HB4(
   .sym_clk_en(sym_clk_ena),
   .clock_12_5_en(sys_clk),
   .x_in(y_inter2),
-  .y(y_inter3),
-  .reset(reset)
-);
-
-always @ (posedge clock_12_5_en)
-  y_inter3_delay <= y_inter3;
-
-halfband_filter_decim HB3(
-  // .clk(sys_clk),
-  .clk(clock_12_5_en),
-  .sam_clk_en(sam_clk_ena),
-  .sym_clk_en(sym_clk_ena),
-  .clock_12_5_en(clock_12_5_en),
-  .x_in(y_inter3_delay),
   .y(y),
   .reset(reset)
 );
+
+// always @ (posedge clock_12_5_en)
+//   y_inter3_delay <= y_inter3;
+
+// halfband_filter_decim HB3(
+//   // .clk(sys_clk),
+//   .clk(clock_12_5_en),
+//   .sam_clk_en(sam_clk_ena),
+//   .sym_clk_en(sym_clk_ena),
+//   .clock_12_5_en(clock_12_5_en),
+//   .x_in(y_inter3_delay),
+//   .y(y),
+//   .reset(reset)
+// );
 
 
 // tx_pract_filter2 tx(
